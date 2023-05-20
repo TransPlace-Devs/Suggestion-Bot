0565dcf8b5b4c16eb307e496bfd8ed736122db48
SqueakyBeaver <theboxguy659@gmail.com>
2023-05-20 09:29:02 -0500

    Change the embeds to have fields

--- handlers/component/suggestModalSubmit.js
+++ handlers/component/suggestModalSubmit.js
@@ -13,50 +13,48 @@ exports.run = async (client, interaction, options) => {
     }
 
     function isImgur(url) {
-        return /^https:\/\/i\.imgur\.com\//.test(url)
+        return /^https:\/\/i\.imgur\.com\//.test(url);
     }
 
-    const title = escapeMarkdown(options[0].components[0].value)
-    const reason = escapeMarkdown(options[1].components[0].value)
-    const info = escapeMarkdown(options[2].components[0].value)
-    const asset = escapeMarkdown(options[3].components[0].value)
+    const title = escapeMarkdown(options[0].components[0].value);
+    const reason = escapeMarkdown(options[1].components[0].value);
+    const info = escapeMarkdown(options[2].components[0].value);
+    const asset = escapeMarkdown(options[3].components[0].value);
 
-    let extra = ""
-    let extraImage = ""
+    let extra = "";
+    let extraImage = "";
 
-    let image = undefined
-
-    if (info.length > 0) extra = `\`\`\`Additional Information:\n${info}\`\`\``
+    let image = undefined;
 
     let replyEmbed = new EmbedBuilder()
         .setTitle(`Successfully Created Suggestion!`)
-        .setColor(0x5376e0)
+        .setColor(0x5376e0);
 
 
     if (asset.length > 0) {
-        extraImage = `\`\`\`Image/Gif Asset:\n${asset}\`\`\``
-        replyEmbed.setDescription(`\`\`\`Suggestion:\n${title}\`\`\`\`\`\`Reason:\n${reason}\`\`\`${extra}${extraImage}`)
-        if (isImgur(asset)) image = asset
+        extraImage = `\`\`\`Image/Gif Asset:\n${asset}\`\`\``;
+        replyEmbed.setDescription(`\`\`\`Suggestion:\n${title}\`\`\`\`\`\`Reason:\n${reason}\`\`\`${extra}${extraImage}`);
+        if (isImgur(asset)) image = asset;
         else {
-            replyEmbed.setTitle("Attachment URL Invalid:\n***Must Be a valid imgur URL***")
+            replyEmbed.setTitle("Attachment URL Invalid:\n***Must Be a valid imgur URL***");
             // replyEmbed.setDescription(`Make sure the image is one of the following: png/jpg/jpeg/apng/gif.\nCheck that the url is formatted like: (https://i.imgur.com/image.png)\n\nIf you have a URL like (https://imgur.com/a/image):\nRight click the image on imgur and click \"Open image in New Tab\" and copy the url it opens it\n\n(It then should look like the https://i.imgur.com/\n\n\`\`\`Suggestion:\n${title}\`\`\`\`\`\`Reason:\n${reason}\`\`\`${extra}${extraImage})`)
             return interaction.reply({
                 content: "Failed to send suggestion. (Attached is attempted values.)",
                 embeds: [replyEmbed],
                 ephemeral: true,
-            })
+            });
         }
 
     }
 
 
-    const suggestionChannel = interaction.guild.channels.cache.get(process.env.SUGGESTION_CHANNEL)
+    const suggestionChannel = interaction.guild.channels.cache.get(process.env.SUGGESTION_CHANNEL);
 
     if (!suggestionChannel) {
         return interaction.reply({
             content: "Unable to find suggestions channel, this should not occur, please notify server staff of this issue.",
             ephemeral: true,
-        })
+        });
     }
 
     let suggestionEmbed = new EmbedBuilder()
@@ -67,26 +65,37 @@ exports.run = async (client, interaction, options) => {
             })
         })
         .setTitle(`***${title}***`)
-        // TODO: Make this into fields; ask Cleo
-        .setDescription(`\`\`\`Reason:\n${reason}\`\`\`${extra}\nTo join the discussion, check out the thread below :arrow_down:`)
         .setColor(0x5376e0)
-        .setImage(image)
+        .setThumbnail(image)
         .setTimestamp()
         .setFooter({
             text: `/suggest | User ID: ${interaction.user.id}`,
             iconURL: client.user.avatarURL()
-        })
+        });
+
+    if (info.length > 0) {
+        suggestionEmbed.addFields([
+            { name: "Reason:", value: `\`\`\`${reason}\`\`\`` },
+            { name: "Extra Info", value: `\`\`\`${info}\`\`\`` },
+            { name: "Join the Discussion!", value: "Check out the thread below :arrow_down:" },
+        ]);
+    } else {
+        suggestionEmbed.addFields([
+            { name: "Reason:", value: `\`\`\`${reason}\`\`\`` },
+            { name: "Join the Discussion!", value: "Check out the thread below :arrow_down:" },
+        ]);
+    }
 
     const suggestionMessage = await suggestionChannel.send({
         embeds: [suggestionEmbed]
-    })
+    });
 
-    let suggestionThreadTitle = `Suggestion - ${title}`.substring(0, 95)
+    let suggestionThreadTitle = `Suggestion - ${title}`.substring(0, 95);
 
     await suggestionMessage.startThread({
         name: suggestionThreadTitle,
         autoArchiveDuration: 10080,
-    })
+    });
 
     // await suggestionMessage.react(interaction.guild.emojis.cache.get(process.env.UPVOTE))
     //     .catch(console.error);
@@ -100,22 +109,22 @@ exports.run = async (client, interaction, options) => {
     await suggestionMessage.react("⬇️")
         .catch(console.error);
 
-    const suggestionURL = `https://discord.com/channels/${interaction.guild.id}/${suggestionChannel.id}/${suggestionMessage.id}`
+    const suggestionURL = `https://discord.com/channels/${interaction.guild.id}/${suggestionChannel.id}/${suggestionMessage.id}`;
 
 
-    const replyEmbedRow = new ActionRowBuilder()
+    const replyEmbedRow = new ActionRowBuilder();
 
     replyEmbedRow.addComponents([
         (new ButtonBuilder()
             .setLabel("Jump to Suggestion")
             .setURL(suggestionURL)
             .setStyle(5))
-    ])
+    ]);
 
     return interaction.reply({
         embeds: [replyEmbed],
         components: [replyEmbedRow],
         ephemeral: true,
-    })
+    });
 
-}
+};
