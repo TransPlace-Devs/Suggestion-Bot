779ec3e56d4ff16c389eec6bb90b58bd71882136
Alex L <alex.yj.liao@gmail.com>
2024-07-05 04:14:08 -0400

    feat: insert info embed before the suggestion modal (#4)
    We often get repetitive suggestions which we have denied before, and can't consider changing due to safety concerns. To field these off, it's nice to ask the user to confirm that their suggestion doesn't mach these

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
+// IMPORTANT: Only add the partnerships_channel once that channel actually contains a server with a venting channel.
+// That field is just used to provide the details for the "we will not add a venting channel" notice to the
+// suggestion workflow, so it should not direct users to the channel if none of the servers there have a venting channel.
+
 module.exports = {
     "959551566388547676": { // TransPlace
         "suggestion_channel": "977280417877090424",
-        "suggestion_ban_role": "994776459086401576"
+        "suggestion_ban_role": "994776459086401576",
+        "partnerships_channel": "987881457852776528"
     },
     "1087014898199969873": { // EnbyPlace
         "suggestion_channel": "1258486010996592701",
         "suggestion_ban_role": "1258486058425913345"
-    },
+    }
 }
